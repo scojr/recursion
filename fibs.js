2407b692b845e1f222c067bf8dc1a4333705711d
scojr <180873402+scojr@users.noreply.github.com>
2025-04-02 19:29:45 -0500

    Fix fibsIterative return value

--- fibs.js
+++ fibs.js
@@ -5,7 +5,7 @@ function fibsIterative(number) {
     let seqLength = sequence.length;
     sequence.push(sequence[seqLength - 1] + sequence[seqLength - 2])
   }
-  console.log(sequence);
+  return sequence;
 }
 
 // console.log(fibsIterative(8)); // returns [0, 1, 1, 2, 3, 5, 8, 13]
